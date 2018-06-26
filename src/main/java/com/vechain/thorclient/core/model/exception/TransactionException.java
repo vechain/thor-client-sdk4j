5c1590eded75c85c16e1f81a102fe84d412d0a16
QingFeng Pei <qingfeng.pei@vechain.com>
2018-06-26 15:06:32 +0800

    add sponsor test

---
+++ src/main/java/com/vechain/thorclient/core/model/exception/TransactionException.java
@@ -0,0 +1,7 @@
+package com.vechain.thorclient.core.model.exception;
+
+public class TransactionException extends ThorException {
+    public TransactionException(String message) {
+        super(message);
+    }
+}
