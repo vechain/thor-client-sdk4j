e5b008e8f375bf16a9dedce8e90d156dc2bce871
albertma <albert.ma@vechain.com>
2018-07-01 17:12:05 +0800

    1. add deploy contract

--- src/main/java/com/vechain/thorclient/core/model/clients/base/AbstractContract.java
+++ src/main/java/com/vechain/thorclient/core/model/clients/base/AbstractContract.java
@@ -77,6 +77,11 @@ public class AbstractContract {
         return null;
     }
 
+    //todo
+    private AbiDefinition getAbiDefinition(){
+        return null;
+    }
+
 
     private boolean checkInputsType(List<String> inputTypes,  AbiDefinition abiDefinition){
         if(inputTypes == null){
