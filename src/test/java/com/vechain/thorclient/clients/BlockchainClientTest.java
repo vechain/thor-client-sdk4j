cbd6f3f0a1276a71774dfdac15a4d852439bec35
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-04-25 12:54:01 +0100

    BlockChainClientTest...

--- src/test/java/com/vechain/thorclient/clients/BlockchainClientTest.java
+++ src/test/java/com/vechain/thorclient/clients/BlockchainClientTest.java
@@ -7,25 +7,27 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 @RunWith(JUnit4.class)
 public class BlockchainClientTest extends BaseTest {
 
-	@Test
-	public void testGetChainTag() throws ClientArgumentException {
-		byte chainTag = BlockchainClient.getChainTag();
-		int chainTagInt = chainTag & 0xff;
-		logger.info("chainTag: " + chainTagInt);
-		Assert.assertTrue(chainTagInt > 0);
-	}
+    // Galactica tested: http://localhost:8669/doc/stoplight-ui/#/paths/blocks-revision/get
+    // GET http://localhost:8669/blocks/0
+    // Accept: application/json, text/plain
+    @Test
+    public void testGetChainTag() throws ClientArgumentException {
+        byte chainTag = BlockchainClient.getChainTag();
+        int chainTagInt = chainTag & 0xff;
+        logger.info("chainTag: " + chainTagInt);
+        Assert.assertTrue(chainTagInt > 0);
+    }
 
-	@Test
-	public void testGetNodeStats() throws ClientArgumentException {
-		ArrayList list = BlockchainClient.getPeerStatusList();
-		logger.info("nodes list:" + list);
-		// Assert.assertNotNull(list);
-	}
+    @Test
+    public void testGetNodeStats() throws ClientArgumentException {
+        ArrayList list = BlockchainClient.getPeerStatusList();
+        logger.info("nodes list:" + list);
+        // Assert.assertNotNull(list);
+    }
 
 }
