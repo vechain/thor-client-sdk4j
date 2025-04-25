e875a08c6a1d401cfcaaea230e4798311b04fb48
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-04-25 12:01:27 +0100

    mvn fixed

--- src/test/java/com/vechain/thorclient/clients/AccountClientTest.java
+++ src/test/java/com/vechain/thorclient/clients/AccountClientTest.java
@@ -1,13 +1,5 @@
 package com.vechain.thorclient.clients;
 
-import java.io.IOException;
-
-import com.vechain.thorclient.core.model.clients.Revision;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
 import com.alibaba.fastjson.JSON;
 import com.vechain.thorclient.base.BaseTest;
 import com.vechain.thorclient.core.model.blockchain.Account;
@@ -16,21 +8,26 @@ import com.vechain.thorclient.core.model.blockchain.StorageData;
 import com.vechain.thorclient.core.model.clients.Address;
 import com.vechain.thorclient.core.model.clients.StorageKey;
 import com.vechain.thorclient.utils.BytesUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class AccountClientTest extends BaseTest {
 
     @Test
-    public void testGetAccountInfo() throws IOException {
-        Address address = Address.fromHexString("0x3831Fc6ecF6C88D171e6eF3E2F291fE100925617");
-        Account account = AccountClient.getAccountInfo(address, Revision.create( 296759 ));
+    // Galactica tested.
+    public void testGetAccountInfo() {
+        Address address = Address.fromHexString("0x3db469a79593dcc67f07DE1869d6682fC1eaf535");
+        Account account = AccountClient.getAccountInfo(address, null);
         logger.info("account info:" + JSON.toJSONString(account));
         logger.info("VET:" + account.VETBalance().getAmount() + " Energy:" + account.energyBalance().getAmount());
         Assert.assertNotNull(account);
     }
 
     @Test
-    public void testGetStorageAt() throws IOException {
+    public void testGetStorageAt() {
         byte[] address = BytesUtils.toByteArray(fromAddress);
         StorageKey key = StorageKey.create(4, address);
         StorageData data = AccountClient.getStorageAt(Address.VTHO_Address, key, null);
@@ -39,7 +36,8 @@ public class AccountClientTest extends BaseTest {
     }
 
     @Test
-    public void testGetCodeTest() throws IOException {
+    // Galactica tested.
+    public void testGetCodeTest() {
         Address tokenAddr = Address.VTHO_Address;
         AccountCode code = AccountClient.getAccountCode(tokenAddr, null);
         logger.info("code:" + JSON.toJSONString(code));
