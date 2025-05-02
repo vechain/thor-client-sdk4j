4abef3a681b372d0c066cbf0a3e4a6644da14ada
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-05-02 18:31:45 +0100

    ERC20ContractClientTest fixture tested.

--- src/test/java/com/vechain/thorclient/clients/LogsClientTest.java
+++ src/test/java/com/vechain/thorclient/clients/LogsClientTest.java
@@ -20,7 +20,7 @@ import com.vechain.thorclient.utils.Prefix;
 @RunWith(JUnit4.class)
 public class LogsClientTest extends BaseTest {
 
-    boolean prettyFormat = true;
+    boolean prettyFormat = isPretty();
 
     // Galactica documented at: http://127.0.0.1:8669/doc/stoplight-ui/#/paths/logs-event/post
     // Solo tested.
@@ -48,34 +48,31 @@ public class LogsClientTest extends BaseTest {
     //   "order": "desc"
     //}
     @Test
-    public void testFilterEvents() throws ClientArgumentException {
-        Block block = BlockClient.getBlock(null);
-
-        List<String> eventsTransferInputs = new ArrayList<String>();
+    public void testGetFilteredLogEvents() throws ClientArgumentException {
+        final String address = System.getProperty("LogsClientTest.testGetFilteredLogEvents.address");
+        final String topic0 = System.getProperty("LogsClientTest.testGetFilteredLogEvents.topic0");
+        final Block block = BlockClient.getBlock(null);
+        final List<String> eventsTransferInputs = new ArrayList<>();
         eventsTransferInputs.add("address");
         eventsTransferInputs.add("address");
         eventsTransferInputs.add("uint256");
-        AbiDefinition abiDefinition = ERC20Contract.defaultERC20Contract.findAbiDefinition("Transfer", "event",
-                eventsTransferInputs);
-        String abiMethodHexString = BytesUtils.toHexString(abiDefinition.getBytesMethodHashed(), Prefix.ZeroLowerX);
-        logger.info("abi Transfer:" + abiMethodHexString);
-        LogFilter logFilter = LogFilter.createFilter(Range.createBlockRange(0, Long.parseLong(block.getNumber())),
-                Options.create(0, 10));
-        logFilter.addTopicSet(
-                "0x0000000000000000000000000000506172616d73",
-                "0x28e3246f80515f5c1ed987b133ef2f193439b25acba6a5e69f219e896fc9d179",
-                null,
-                null,
-                null,
-                null
+        final AbiDefinition abiDefinition = ERC20Contract.defaultERC20Contract.findAbiDefinition(
+                "Transfer",
+                "event",
+                eventsTransferInputs
         );
+        final String abiMethodHexString = BytesUtils.toHexString(abiDefinition.getBytesMethodHashed(), Prefix.ZeroLowerX);
+        logger.info("abi Transfer:" + abiMethodHexString);
+        final LogFilter logFilter = LogFilter.createFilter(
+                Range.createBlockRange(0, Long.parseLong(block.getNumber())),
+                Options.create(0, 10));
+        logFilter.addTopicSet(address, topic0, null, null, null, null);
         logFilter.setOrder(Order.DESC.getValue());
-
-        ArrayList<FilteredLogEvent> filteredEvents = LogsClient.getFilteredLogEvents(logFilter);
+        final ArrayList<FilteredLogEvent> filteredEvents = LogsClient.getFilteredLogEvents(logFilter);
         for (FilteredLogEvent filteredTransferEvent : filteredEvents) {
             logger.info("filteredTransferEvent:{}", JSONObject.toJSONString(filteredTransferEvent, prettyFormat));
             LogMeta lm = filteredTransferEvent.getMeta();
-            logger.info(JSONObject.toJSONString(lm, true));
+            logger.info(JSONObject.toJSONString(lm, prettyFormat));
         }
     }
 
@@ -105,16 +102,20 @@ public class LogsClientTest extends BaseTest {
     //  "order": "desc"
     // }
     @Test
-    public void testTransferLogs() throws ClientArgumentException {
-        Block block = BlockClient.getBlock(null);
-        TransferredFilter transferredFilter = TransferredFilter
-                .createFilter(Range.createBlockRange(0, Long.parseLong(block.getNumber())), Options.create(0, 10));
+    public void testGetFilteredTransferLogs() throws ClientArgumentException {
+        final String txOrigin = System.getProperty("LogsClientTest.testGetFilteredTransferLogs.txOrigin");
+        final String sender = System.getProperty("LogsClientTest.testGetFilteredTransferLogs.sender");
+        final Block block = BlockClient.getBlock(null);
+        final TransferredFilter transferredFilter = TransferredFilter.createFilter(
+                Range.createBlockRange(0, Long.parseLong(block.getNumber())),
+                Options.create(0, 10)
+        );
         transferredFilter.addTransferCriteria(
-                Address.fromHexString("0xf077b491b355e64048ce21e3a6fc4751eeea77fa"),
-                Address.fromHexString("0xf077b491b355e64048ce21e3a6fc4751eeea77fa"),
+                Address.fromHexString(txOrigin),
+                Address.fromHexString(sender),
                 null);
         transferredFilter.setOrder(Order.DESC.getValue());
-        ArrayList<FilteredTransferEvent> transferLogs = LogsClient.getFilteredTransferLogs(transferredFilter);
+        final ArrayList<FilteredTransferEvent> transferLogs = LogsClient.getFilteredTransferLogs(transferredFilter);
         logger.info("transferLogs:{}", JSONObject.toJSONString(transferLogs, prettyFormat));
     }
 
