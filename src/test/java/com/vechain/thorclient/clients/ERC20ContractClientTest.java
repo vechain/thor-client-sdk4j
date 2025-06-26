293d20a7e6987c7303a58a55cab0f44a8ddaec7c
Andreas Frank <andreas.frank@MacBook-Pro-Andreas.local>
2025-06-26 12:44:46 +0300

    Swicthed to raw trx

--- src/test/java/com/vechain/thorclient/clients/ERC20ContractClientTest.java
+++ src/test/java/com/vechain/thorclient/clients/ERC20ContractClientTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.vechain.thorclient.utils.Prefix;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -16,6 +17,8 @@ import com.vechain.thorclient.core.model.clients.Address;
 import com.vechain.thorclient.core.model.clients.Amount;
 import com.vechain.thorclient.core.model.clients.ERC20Token;
 import com.vechain.thorclient.utils.crypto.ECKeyPair;
+
+import java.math.BigInteger;
 
 @RunWith(JUnit4.class)
 public class ERC20ContractClientTest extends BaseTest {
@@ -48,6 +51,7 @@ public class ERC20ContractClientTest extends BaseTest {
     // Galactica documented at:
     // http://127.0.0.1:8669/doc/stoplight-ui/#/paths/accounts-*/post
     // Solo tested.
+
     @Test
     public void testTransferERC20Token() throws JsonProcessingException {
         // Set in `config.properties`.
@@ -125,4 +129,85 @@ public class ERC20ContractClientTest extends BaseTest {
         Assert.assertTrue(toBalanceAfter.getAmount().compareTo(toBalanceBefore.getAmount()) > 0);
     }
 
+    // Tests ERC20 token transfer using dynamic fee (EIP-1559) transaction
+    @Test
+    public void testTransferERC20TokenWithDynamicFee() throws JsonProcessingException {
+        // Set in `config.properties`.
+        final String fromPrivateKey = System.getProperty("ERC20ContractClientTest.testTransferERC20Token.fromKey");
+        final Address fromAddress = Address.fromBytes(ECKeyPair.create(fromPrivateKey).getRawAddress());
+        // Set in `config.properties`.
+        final Address toAddress = Address
+                .fromHexString(System.getProperty("ERC20ContractClientTest.testTransferERC20Token.toAddress"));
+        final String amountFigure = "10000";
+        final Amount amount = Amount.VTHO();
+        amount.setDecimalAmount(amountFigure);
+
+        final Amount fromBalanceBefore = ERC20ContractClient.getERC20Balance(fromAddress, ERC20Token.VTHO, null);
+        if (fromBalanceBefore != null) {
+            final String message = String.format(
+                    "BEFORE: Account %s: VTHO %s.",
+                    fromAddress.toHexString(Prefix.ZeroLowerX),
+                    fromBalanceBefore.getAmount().toString());
+            logger.info(message);
+        } else {
+            Assert.fail(String.format("Account %s not found!", fromAddress.toHexString(Prefix.ZeroLowerX)));
+        }
+        final Amount toBalanceBefore = ERC20ContractClient.getERC20Balance(toAddress, ERC20Token.VTHO, null);
+        if (toBalanceBefore != null) {
+            final String message = String.format(
+                    "BEFORE: Account %s: VTHO %s.",
+                    toAddress.toHexString(Prefix.ZeroLowerX),
+                    toBalanceBefore.getAmount().toString());
+            logger.info(message);
+        } else {
+            Assert.fail(String.format("Account %s not found!", toAddress.toHexString(Prefix.ZeroLowerX)));
+        }
+
+        BigInteger maxFeePerGas = new BigInteger("5000000000"); // 5 gwei
+        BigInteger maxPriorityFeePerGas = new BigInteger("1000000000"); // 1 gwei
+
+        final TransferResult transferResult = ERC20ContractClient.transferERC20TokenEIP1559(
+                new Address[] { toAddress },
+                new Amount[] { amount },
+                1000000,
+                maxFeePerGas,
+                maxPriorityFeePerGas,
+                720,
+                ECKeyPair.create(fromPrivateKey));
+        logger.info("transferERC20Token: {}", writer.writeValueAsString(transferResult));
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            final String message = String.format("InterruptedException: %s", e.getMessage());
+            logger.error(message);
+            Assert.fail(message);
+        }
+
+        final Receipt receipt = TransactionClient.getTransactionReceipt(transferResult.getId(), null);
+        logger.info("Receipt: {}", writer.writeValueAsString(receipt));
+
+        final Amount fromBalanceAfter = ERC20ContractClient.getERC20Balance(fromAddress, ERC20Token.VTHO, null);
+        if (fromBalanceAfter != null) {
+            final String message = String.format(
+                    "AFTER: Account %s: VTHO %s.",
+                    fromAddress.toHexString(Prefix.ZeroLowerX),
+                    fromBalanceAfter.getAmount().toString());
+            logger.info(message);
+        } else {
+            Assert.fail(String.format("Account %s not found!", fromAddress.toHexString(Prefix.ZeroLowerX)));
+        }
+        final Amount toBalanceAfter = ERC20ContractClient.getERC20Balance(toAddress, ERC20Token.VTHO, null);
+        if (toBalanceAfter != null) {
+            String message = String.format(
+                    "AFTER: Account %s: VTHO %s.",
+                    toAddress.toHexString(Prefix.ZeroLowerX),
+                    toBalanceAfter.getAmount().toString());
+            logger.info(message);
+        } else {
+            Assert.fail(String.format("Account %s not found!", toAddress.toHexString(Prefix.ZeroLowerX)));
+        }
+        Assert.assertTrue(fromBalanceAfter.getAmount().compareTo(fromBalanceBefore.getAmount()) < 0);
+        Assert.assertTrue(toBalanceAfter.getAmount().compareTo(toBalanceBefore.getAmount()) > 0);
+    }
 }
